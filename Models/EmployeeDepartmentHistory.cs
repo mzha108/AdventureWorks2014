b9dd85f46affe85496d64c8578c647cf984f18e5
Mu <mzha108@gmail.com>
2021-06-07 22:53:59 +1200

    update code

--- Models/EmployeeDepartmentHistory.cs
+++ Models/EmployeeDepartmentHistory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AdventureWorks2014.Models
 {
@@ -8,8 +9,12 @@ namespace AdventureWorks2014.Models
         public int BusinessEntityId { get; set; }
         public short DepartmentId { get; set; }
         public byte ShiftId { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; } = DateTime.Today;
+        [DataType(DataType.Date)]
         public DateTime? EndDate { get; set; }
+        [DataType(DataType.Date)]
         public DateTime ModifiedDate { get; set; } = DateTime.Today;
 
         public Department Department { get; set; }
